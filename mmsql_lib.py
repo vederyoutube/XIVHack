b5917b12c4e600c053a30e437a4f5b57fbad9ab5
vederyoutube <moskvitin.228@gmail.com>
2024-04-20 13:30:10 +0300

    very final ver of lib

--- mmsql_lib.py
+++ mmsql_lib.py
@@ -3,6 +3,7 @@ from mysql.connector import Error
 
 
 log = None
+teamN = None
 
 def create_connection(host_name, user_name, user_password, db_name):
     connection = None
@@ -35,6 +36,7 @@ def user_reg(login, passwd):
         print(f"successful registred user ({login}, {passwd})")
         con.commit()
         con.close()
+        global log
         log = login
         return True
     else:
@@ -53,9 +55,15 @@ def sign_in(login, passwd):
     res = cursor.fetchall()
     print(res)
     if len(res) == 1:
-        con.close()
+        global log, teamN
         log = login
+        query = "SELECT Team_name FROM User_reg WHERE login = (%s)"
+        cursor.execute(query,(log, ))
+        res2 = cursor.fetchone()
+        print(res2)
+        teamN = res2[0]
         print("Log in successful")
+        con.close()
         return True
     else:
         con.close()
@@ -93,6 +101,8 @@ def team_reg(name, mail, login, passwd, mem_list):
                 # cursor.execute('INSERT INTO User_reg (login, password) VALUES (?, ?)', (login, passwd))
                 query = "UPDATE User_reg SET Team_name = %s WHERE login = %s"
                 cursor.execute(query,(name, login))
+                global teamN
+                teamN = name
                 query = "UPDATE User_reg SET Team_mail = %s WHERE login = %s"
                 cursor.execute(query,(mail, login))
                 print(f"successful registred team ({name}, {mail})")
@@ -123,6 +133,55 @@ def mem_reg(name, desc, team_name):
         print("member alredy used")
         con.close()
         return False
-test_ar = [["test", "test1"],["test", "test"]]
+
+def update_team(team_name = None, team_mail = None, team_pass = None, team_login = None,Cteam_name = None, member_name = None,Nmember_name = None, member_desc = None):
+    con = create_connection("a0947494.xsph.ru", "a0947494_teams", "HzqIAQEc", "a0947494_teams")
+    cursor = con.cursor()
+    if team_name:
+        query = "UPDATE User_reg SET Team_name = %s WHERE login = %s"
+        cursor.execute(query,(team_name, log))
+        con.commit()
+        query = "UPDATE Member_list SET Team_name = %s WHERE Team_name = %s"
+        cursor.execute(query,(team_name, Cteam_name))
+        con.commit()
+        global teamN
+        teamN = team_name
+        con.close()
+    
+    if team_mail:
+        query = "UPDATE User_reg SET Team_mail = %s WHERE login = %s"
+        cursor.execute(query,(team_mail, log))
+        con.commit()
+        con.close()
+
+    if team_pass:
+        query = "UPDATE User_reg SET password = %s WHERE login = %s"
+        pass_md5 = hashlib.md5(bytes(team_pass, 'utf-8'))
+        pass_hash = pass_md5.hexdigest()
+        cursor.execute(query,(pass_hash, log))
+        con.commit()
+        con.close()    
+
+    if team_login:
+        query = "UPDATE User_reg SET login = %s WHERE login = %s"
+        cursor.execute(query,(team_login, log))
+        con.commit()
+        log = team_login
+        con.close()    
+
+    if Nmember_name:
+        query = "UPDATE Member_list SET Member_name = %s WHERE Team_name = %s AND Member_name = %s"
+        cursor.execute(query,(Nmember_name, teamN, member_name))
+        con.commit()
+        con.close()
+
+    if member_desc:
+        query = "UPDATE Member_list SET Member_desc = %s WHERE Team_name = %s AND Member_name = %s"
+        cursor.execute(query,(member_desc, teamN, member_name))
+        con.commit()
+        con.close()
+
+# test_ar = [["test", "test1"],["test", "test"]]
 # team_reg("test", "test", "test", "test", test_ar)
-# sign_in("test", "test")
+sign_in("test2", "test2")
+update_team(Nmember_name="test2", member_name="test1")
